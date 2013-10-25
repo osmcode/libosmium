e05644a9b1e66abfda449b00b71a4a5417e80d0b
Jochen Topf <jochen@topf.org>
2013-10-25 21:00:03 +0200

    Make sure we don't queue too much

--- include/osmium/io/opl_output.hpp
+++ include/osmium/io/opl_output.hpp
@@ -238,6 +238,9 @@ namespace osmium {
             void handle_buffer(osmium::memory::Buffer&& buffer) override {
                 OPLOutputBlock output_block(std::move(buffer));
                 m_output_queue.push(osmium::thread::Pool::instance().submit(std::move(output_block)));
+                while (m_output_queue.size() > 10) {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // XXX
+                }
             }
 
             void close() {
