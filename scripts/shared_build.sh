22eda411fca55b51708a601fc8211547ab6aa838
Dane Springmeyer <dane@mapbox.com>
2014-04-30 18:58:20 -0700

    ensure g++ is upgraded for shared build

--- scripts/shared_build.sh
+++ scripts/shared_build.sh
@@ -5,8 +5,12 @@ if [ ${UNAME} = 'Darwin' ]; then
     brew install boost protobuf osm-pbf expat google-sparsehash
 else
     # install packages
+    sudo apt-add-repository --yes ppa:ubuntu-toolchain-r/test
     sudo apt-add-repository --yes ppa:mapnik/boost # boost 1.49 (in future 1.55)
     sudo apt-get -qq update
+    sudo apt-get -y install gcc-4.8 g++-4.8
+    export CXX=g++-4.8
+    export CC=gcc-4.8
     sudo apt-get -y install git build-essential libboost-dev zlib1g-dev protobuf-compiler libprotobuf-lite7 libprotobuf-dev libexpat1-dev libsparsehash-dev
 
     # install osmpbf
