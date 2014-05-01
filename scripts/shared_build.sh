441e56c9a173bdb95adb104ad158ca537d82748f
Dane Springmeyer <dane@mapbox.com>
2014-04-30 18:53:03 -0700

    fixup build scripts

--- scripts/shared_build.sh
+++ scripts/shared_build.sh
@@ -2,23 +2,12 @@
 
 UNAME=$(uname -s);
 if [ ${UNAME} = 'Darwin' ]; then
-    brew install boost cmake protobuf libstxxl lua luabind osm-pbf
+    brew install boost protobuf osm-pbf expat google-sparsehash
 else
     # install packages
     sudo apt-add-repository --yes ppa:mapnik/boost # boost 1.49 (in future 1.55)
     sudo apt-get -qq update
-    sudo apt-get install -y libboost-filesystem-dev libboost-program-options-dev libboost-iostreams-dev libboost-regex-dev libboost-system-dev libboost-thread-dev
-    sudo apt-get install -y build-essential git cmake libprotoc-dev libprotobuf7 protobuf-compiler libprotobuf-dev libbz2-dev libstxxl-dev libstxxl-doc libstxxl1 libxml2-dev libzip-dev lua5.1 liblua5.1-0-dev
-
-    # install luabind
-    git clone --depth=1 https://github.com/DennisOSRM/luabind.git
-    cd luabind
-    mkdir -p build
-    cd build
-    cmake ../ -DCMAKE_BUILD_TYPE=Release
-    make
-    sudo make install
-    cd ../../
+    sudo apt-get -y install git build-essential libboost-dev zlib1g-dev protobuf-compiler libprotobuf-lite7 libprotobuf-dev libexpat1-dev libsparsehash-dev
 
     # install osmpbf
     git clone --depth=1 https://github.com/scrosby/OSM-binary.git
@@ -26,19 +15,4 @@ else
     make
     sudo make install
     cd ../../
-
 fi
-
-# install OSRM
-git clone --depth=1 https://github.com/DennisOSRM/Project-OSRM.git Project-OSRM -b develop
-cd Project-OSRM
-mkdir -p build 
-cd build
-cmake ../ -DWITH_TOOLS=1
-make
-sudo make install
-cd ../../
-pkg-config libosrm --cflags
-pkg-config libosrm --libs
-pkg-config libosrm --libs --static
-cat /usr/local/lib/pkgconfig/*
--- scripts/static_build.sh
+++ scripts/static_build.sh
@@ -7,22 +7,11 @@ if [[ ${TMP_DEPS_DIR:-false} == false ]]; then
     mkdir -p ${TMP_DEPS_DIR}
 fi
 if [[ ${NODE_MODULE_ROOT:-false} == false ]]; then
-    NODE_MODULE_ROOT=`pwd`
+    NODE_MODULE_ROOT=$(pwd)
 fi
-git clone --depth=1 https://github.com/mapnik/mapnik-packaging.git  ${TMP_DEPS_DIR}/mapnik-packaging
+git clone --depth=1 https://github.com/mapnik/mapnik-packaging.git ${TMP_DEPS_DIR}/mapnik-packaging
 cd ${TMP_DEPS_DIR}/mapnik-packaging
-export CXX11=false
+export CXX11=true
 source build.sh
-build_osrm
+build_osmium
 cd ${NODE_MODULE_ROOT}
-if [[ ${PACKAGE_COMMAND_LINE_TOOLS:-false} != false ]]; then
-    ls ${NODE_MODULE_ROOT}
-    ls ${NODE_MODULE_ROOT}/lib/binding/
-    mkdir -p "${NODE_MODULE_ROOT}/lib/binding/"
-    cp `which lua` "${NODE_MODULE_ROOT}/lib/binding/"
-    cp `which osrm-extract` "${NODE_MODULE_ROOT}/lib/binding/"
-    cp `which osrm-prepare` "${NODE_MODULE_ROOT}/lib/binding/"
-    cp `which osrm-datastore` "${NODE_MODULE_ROOT}/lib/binding/"
-else
-    echo "not packaging command line tools"
-fi
