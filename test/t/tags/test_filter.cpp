2b2fd4af3852baf6966f2688c57443c3ea20a6bc
Jochen Topf <jochen@topf.org>
2014-06-19 16:30:06 +0200

    Use create_tag_list() function in tests.

--- test/t/tags/test_filter.cpp
+++ test/t/tags/test_filter.cpp
@@ -3,10 +3,10 @@
 #endif
 #include <boost/test/unit_test.hpp>
 
-#include <osmium/builder/osm_object_builder.hpp>
 #include <osmium/memory/buffer.hpp>
 #include <osmium/osm/tag.hpp>
 #include <osmium/osm/ostream.hpp>
+#include <osmium/tags/taglist.hpp>
 #include <osmium/tags/key_filter.hpp>
 #include <osmium/tags/key_value_filter.hpp>
 
@@ -18,15 +18,11 @@ BOOST_AUTO_TEST_CASE(KeyFilter) {
     filter.add(true, "highway").add(true, "railway");
 
     osmium::memory::Buffer buffer(10240);
-    {
-        osmium::builder::TagListBuilder tl_builder(buffer);
-        tl_builder.add_tag("highway", "primary");
-        tl_builder.add_tag("name", "Main Street");
-        tl_builder.add_tag("source", "GPS");
-    }
-    buffer.commit();
-
-    const osmium::TagList& tl = buffer.get<const osmium::TagList>(0);
+    const osmium::TagList& tl = osmium::tags::create_tag_list(buffer, {
+        { "highway", "primary" },
+        { "name", "Main Street" },
+        { "source", "GPS" }
+    });
 
     auto tagit = tl.begin();
     BOOST_CHECK_EQUAL(filter(*tagit), true);
@@ -47,15 +43,12 @@ BOOST_AUTO_TEST_CASE(KeyValueFilter) {
     filter.add(true, "highway", "residential").add(true, "highway", "primary").add(true, "railway");
 
     osmium::memory::Buffer buffer(10240);
-    {
-        osmium::builder::TagListBuilder tl_builder(buffer);
-        tl_builder.add_tag("highway", "primary");
-        tl_builder.add_tag("railway", "tram");
-        tl_builder.add_tag("source", "GPS");
-    }
-    buffer.commit();
+    const osmium::TagList& tl = osmium::tags::create_tag_list(buffer, {
+        { "highway", "primary" },
+        { "railway", "tram" },
+        { "source", "GPS" }
+    });
 
-    const osmium::TagList& tl = buffer.get<const osmium::TagList>(0);
     const osmium::Tag& t1 = *(tl.begin());
     const osmium::Tag& t2 = *(std::next(tl.begin(), 1));
     const osmium::Tag& t3 = *(std::next(tl.begin(), 2));
