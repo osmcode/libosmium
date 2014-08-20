c74f72f9d6a0928c5bca3a459055329c5041f021
alex85k <alex85k@github.com>
2014-08-20 15:01:18 +0600

    replace missing gmtime_r on Windows

--- include/osmium/osm/timestamp.hpp
+++ include/osmium/osm/timestamp.hpp
@@ -39,6 +39,7 @@ DEALINGS IN THE SOFTWARE.
 #include <limits>
 #include <stdexcept>
 #include <string>
+#include <time.h>
 
 namespace osmium {
 
@@ -117,11 +118,14 @@ namespace osmium {
             if (m_timestamp == 0) {
                 return std::string("");
             }
-            struct tm tm {
-                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-            };
+            struct tm tm;
             time_t sse = seconds_since_epoch();
+#ifndef _MSC_VER
             gmtime_r(&sse, &tm);
+#else
+            gmtime_s(&tm, &sse);
+#endif
+
             std::string s(timestamp_length, '\0');
             /* This const_cast is ok, because we know we have enough space
                in the string for the format we are using (well at least until
