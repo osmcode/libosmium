167bd437459d4bf92373cd71d91a4012b9a886bf
Jochen Topf <jochen@topf.org>
2014-09-10 16:18:50 +0200

    Add tests showing OSM objects can't be created from Javascript.

---
+++ test/buffer.test.js
@@ -0,0 +1,63 @@
+var osmium = require('../');
+var assert = require('assert');
+
+describe('buffer', function() {
+
+    it('should not be able to create an osmium.OSMObject from Javascript', function(done) {
+        var got_exception = false;
+        try {
+            var object = new osmium.OSMObject;
+        } catch (e) {
+            assert.ok(e instanceof TypeError);
+            assert.equal(e.message, 'osmium.OSMObject cannot be created in Javascript');
+            got_exception = true;
+        } finally {
+            assert.ok(got_exception);
+            done();
+        }
+    });
+
+    it('should not be able to create an osmium.Node from Javascript', function(done) {
+        var got_exception = false;
+        try {
+            var node = new osmium.Node;
+        } catch (e) {
+            assert.ok(e instanceof TypeError);
+            assert.equal(e.message, 'osmium.Node cannot be created in Javascript');
+            got_exception = true;
+        } finally {
+            assert.ok(got_exception);
+            done();
+        }
+    });
+
+    it('should not be able to create an osmium.Way from Javascript', function(done) {
+        var got_exception = false;
+        try {
+            var way = new osmium.Way;
+        } catch (e) {
+            assert.ok(e instanceof TypeError);
+            assert.equal(e.message, 'osmium.Way cannot be created in Javascript');
+            got_exception = true;
+        } finally {
+            assert.ok(got_exception);
+            done();
+        }
+    });
+
+    it('should not be able to create an osmium.Relation from Javascript', function(done) {
+        var got_exception = false;
+        try {
+            var relation = new osmium.Relation;
+        } catch (e) {
+            assert.ok(e instanceof TypeError);
+            assert.equal(e.message, 'osmium.Relation cannot be created in Javascript');
+            got_exception = true;
+        } finally {
+            assert.ok(got_exception);
+            done();
+        }
+    });
+
+});
+
