184b00325fd45252ce7e97003506b3a20b33c111
alex85k <alex85k@github.com>
2014-09-17 17:38:54 +0600

    update batch file for testdata-multipolygon

--- test/osm-testdata/run-testdata-multipolygon.bat
+++ test/osm-testdata/run-testdata-multipolygon.bat
@@ -1,6 +1,7 @@
 set TESTDATA_DIR=%1
 
 del multipolygon.db multipolygon-tests.json
-%2\testdata-multipolygon %TESTDATA_DIR%\grid\data\all.osm 2>&1 >multipolygon.log
+%2\testdata-multipolygon %TESTDATA_DIR%\grid\data\all.osm >multipolygon.log 2>&1
 if ERRORLEVEL 1 (exit /b 1)
-%TESTDATA_DIR%\bin\compare-areas.rb %TESTDATA_DIR%\grid\data\tests.json multipolygon-tests.json
+ruby %TESTDATA_DIR%\bin\compare-areas.rb %TESTDATA_DIR%\grid\data\tests.json multipolygon-tests.json
+if ERRORLEVEL 1 (exit /b 1)
