f1f30139775e238f6f0a02148ac2652f9e94502b
Dane Springmeyer <dane@mapbox.com>
2014-09-25 08:52:05 -0700

    travis: don't upgrade boost for shared build to ensure we test our docs - refs aaf22af1029796

--- scripts/shared_build.sh
+++ scripts/shared_build.sh
@@ -6,12 +6,11 @@ if [ ${UNAME} = 'Darwin' ]; then
 else
     # install packages
     sudo apt-add-repository --yes ppa:ubuntu-toolchain-r/test
-    sudo apt-add-repository --yes ppa:mapnik/boost # boost 1.49 (in future 1.55)
     sudo apt-get -qq update
     sudo apt-get -y install gcc-4.8 g++-4.8
     export CXX=g++-4.8
     export CC=gcc-4.8
-    sudo apt-get -y install git build-essential libboost-dev zlib1g-dev protobuf-compiler libprotobuf-lite7 libprotobuf-dev libexpat1-dev libsparsehash-dev
+    sudo apt-get -y install git build-essential zlib1g-dev protobuf-compiler libprotobuf-lite7 libprotobuf-dev libexpat1-dev libsparsehash-dev
 
     # install osmpbf
     git clone --depth=1 https://github.com/scrosby/OSM-binary.git
