554a57102e2ef112fb0f7bb948c5bbb92ecbdc91
Dane Springmeyer <dane@mapbox.com>
2014-09-25 09:07:11 -0700

    travis: still install boost if not from ppa upgrade

--- scripts/shared_build.sh
+++ scripts/shared_build.sh
@@ -10,7 +10,7 @@ else
     sudo apt-get -y install gcc-4.8 g++-4.8
     export CXX=g++-4.8
     export CC=gcc-4.8
-    sudo apt-get -y install git build-essential zlib1g-dev protobuf-compiler libprotobuf-lite7 libprotobuf-dev libexpat1-dev libsparsehash-dev
+    sudo apt-get -y install git build-essential libboost-dev zlib1g-dev protobuf-compiler libprotobuf-lite7 libprotobuf-dev libexpat1-dev libsparsehash-dev
 
     # install osmpbf
     git clone --depth=1 https://github.com/scrosby/OSM-binary.git
