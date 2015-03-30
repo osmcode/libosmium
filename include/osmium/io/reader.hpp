2b48945c8c3351f9ba44de1e5047abf9cfc4bc95
Jochen Topf <jochen@topf.org>
2015-03-30 22:12:57 +0200

    Fix comment.

--- include/osmium/io/reader.hpp
+++ include/osmium/io/reader.hpp
@@ -260,7 +260,7 @@ namespace osmium {
 
                 // m_input->read() can return an invalid buffer to signal EOF,
                 // or a valid buffer with or without data. A valid buffer
-                // without data is not an error, it just means we have to get
+                // without data is not an error, it just means we have to
                 // keep getting the next buffer until there is one with data.
                 while (true) {
                     osmium::memory::Buffer buffer = m_input->read();
