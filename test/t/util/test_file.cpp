a382274f0ee1cdade12d2ff58d7705bbb8b3f52a
Jochen Topf <jochen@topf.org>
2015-05-20 09:38:13 +0200

    Add invalid parameter handler on Windows to test.

--- test/t/util/test_file.cpp
+++ test/t/util/test_file.cpp
@@ -15,7 +15,23 @@ TEST_CASE("pagesize") {
 
 }
 
+#ifdef _WIN32
+void invalid_parameter_handler(
+   const wchar_t* expression,
+   const wchar_t* function,
+   const wchar_t* file,
+   unsigned int line,
+   uintptr_t pReserved
+) {
+    // do nothing
+}
+#endif
+
 TEST_CASE("file_size") {
+
+#ifdef _WIN32
+   _invalid_parameter_handler old_handler = _set_invalid_parameter_handler(invalid_parameter_handler);
+#endif
 
     SECTION("illegal fd should throw") {
         REQUIRE_THROWS_AS(osmium::util::file_size(-1), std::system_error);
@@ -25,6 +41,10 @@ TEST_CASE("file_size") {
         // its unlikely that fd 1000 is open...
         REQUIRE_THROWS_AS(osmium::util::file_size(1000), std::system_error);
     }
+
+#ifdef _WIN32
+   _set_invalid_parameter_handler(old_handler);
+#endif
 
 }
 
