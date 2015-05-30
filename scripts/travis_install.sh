07fc9b93eed93ba9f11e14517b274b8d713674b4
Jochen Topf <jochen@topf.org>
2015-05-30 11:38:25 +0200

    libsparsehash-dev now in travis package whitelist

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -6,7 +6,7 @@
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 
     # install dependencies
-    sudo apt-get install --yes make libsparsehash-dev libgdal-dev spatialite-bin
+    sudo apt-get install --yes make libgdal-dev spatialite-bin
 
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
 
