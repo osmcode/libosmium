da08073f676cff8aad6fcfcc5fcc67b5f1c6901c
Jochen Topf <jochen@topf.org>
2015-06-26 14:59:10 +0200

    Add min_op/max_op utility functions.

--- include/osmium/osm/timestamp.hpp
+++ include/osmium/osm/timestamp.hpp
@@ -42,6 +42,7 @@ DEALINGS IN THE SOFTWARE.
 #include <time.h>
 
 #include <osmium/util/compatibility.hpp>
+#include <osmium/util/minmax.hpp>
 
 namespace osmium {
 
@@ -168,6 +169,16 @@ namespace osmium {
     inline std::basic_ostream<TChar, TTraits>& operator<<(std::basic_ostream<TChar, TTraits>& out, Timestamp timestamp) {
         out << timestamp.to_iso();
         return out;
+    }
+
+    template <>
+    inline osmium::Timestamp min_op_start_value<osmium::Timestamp>() {
+        return end_of_time();
+    }
+
+    template <>
+    inline osmium::Timestamp max_op_start_value<osmium::Timestamp>() {
+        return start_of_time();
     }
 
 } // namespace osmium
