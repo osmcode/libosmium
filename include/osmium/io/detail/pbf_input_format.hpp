f0d2bcffc0c088da378c9fb045cbed20bdc873d3
Jochen Topf <jochen@topf.org>
2015-07-09 18:20:49 +0200

    Rename pbf_parser.hpp to pbf_decoder.hpp.

--- include/osmium/io/detail/pbf_input_format.hpp
+++ include/osmium/io/detail/pbf_input_format.hpp
@@ -53,7 +53,7 @@ DEALINGS IN THE SOFTWARE.
 
 #include <osmium/io/detail/input_format.hpp>
 #include <osmium/io/detail/pbf.hpp> // IWYU pragma: export
-#include <osmium/io/detail/pbf_parser.hpp>
+#include <osmium/io/detail/pbf_decoder.hpp>
 #include <osmium/io/error.hpp>
 #include <osmium/io/file.hpp>
 #include <osmium/io/file_format.hpp>
