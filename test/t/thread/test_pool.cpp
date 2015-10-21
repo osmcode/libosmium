10dd14fd66220024253bfc08833533f90625bf43
Jochen Topf <jochen@topf.org>
2015-10-21 11:13:49 +0200

    Remove threading test that fails when machine is too busy.

--- test/t/thread/test_pool.cpp
+++ test/t/thread/test_pool.cpp
@@ -6,14 +6,6 @@
 
 #include <osmium/thread/pool.hpp>
 
-static std::atomic<int> result;
-
-struct test_job_ok {
-    void operator()() const {
-        result = 1;
-    }
-};
-
 struct test_job_with_result {
     int operator()() const {
         return 42;
@@ -28,37 +20,19 @@ struct test_job_throw {
 
 TEST_CASE("thread") {
 
+    auto& pool = osmium::thread::Pool::instance();
+
     SECTION("can get access to thread pool") {
-        auto& pool = osmium::thread::Pool::instance();
         REQUIRE(pool.queue_empty());
     }
 
     SECTION("can send job to thread pool") {
-        auto& pool = osmium::thread::Pool::instance();
-        result = 0;
-        auto future = pool.submit(test_job_ok {});
-
-        // wait a bit for the other thread to get a chance to run
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-
-        REQUIRE(result == 1);
-
-        future.get();
-
-        REQUIRE(true);
-    }
-
-    SECTION("can send job to thread pool") {
-        auto& pool = osmium::thread::Pool::instance();
         auto future = pool.submit(test_job_with_result {});
 
         REQUIRE(future.get() == 42);
     }
 
     SECTION("can throw from job in thread pool") {
-        auto& pool = osmium::thread::Pool::instance();
-        result = 0;
-
         auto future = pool.submit(test_job_throw {});
 
         REQUIRE_THROWS_AS(future.get(), std::runtime_error);
