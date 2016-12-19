bffe6269abf98868b31e8b6fa7e0c700bd2993c9
Dmitry Marakasov <amdmi3@amdmi3.ru>
2016-12-19 10:55:24 +0300

    Handle endianess on FreeBSD properly

--- include/osmium/util/endian.hpp
+++ include/osmium/util/endian.hpp
@@ -35,7 +35,9 @@ DEALINGS IN THE SOFTWARE.
 
 // Windows is only available for little endian architectures
 // http://stackoverflow.com/questions/6449468/can-i-safely-assume-that-windows-installations-will-always-be-little-endian
-#if !defined(_WIN32) && !defined(__APPLE__)
+#if defined(__FreeBSD__)
+# include <sys/endian.h>
+#elif !defined(_WIN32) && !defined(__APPLE__)
 # include <endian.h>
 #else
 # define __LITTLE_ENDIAN 1234
