ee9b598edcc4039620a21cd0e4bad997c9bdc893
Jochen Topf <jochen@topf.org>
2017-08-31 11:14:05 +0200

    Add tests for thread_handler.

--- test/t/thread/test_util.cpp
+++ test/t/thread/test_util.cpp
@@ -1,6 +1,7 @@
 #include <catch.hpp>
 
 #include <stdexcept>
+#include <type_traits>
 
 #include <osmium/thread/util.hpp>
 
@@ -33,5 +34,27 @@ TEST_CASE("check_for_exception with exception") {
     }
 
     REQUIRE_THROWS_AS(osmium::thread::check_for_exception(f), const std::runtime_error&);
+}
+
+static_assert(std::is_nothrow_move_constructible<osmium::thread::thread_handler>::value, "thread_handler must have noexcept move constructor");
+
+TEST_CASE("empty thread_handler") {
+    osmium::thread::thread_handler th;
+}
+
+int foo;
+
+void test_func(int value) {
+    foo = value;
+}
+
+TEST_CASE("valid thread_handler") {
+    foo = 22;
+    test_func(17);
+    REQUIRE(foo == 17);
+    {
+        osmium::thread::thread_handler th{test_func, 5};
+    }
+    REQUIRE(foo == 5);
 }
 
