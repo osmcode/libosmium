6fdda54ae7ed0e9bb5278736c9535ac8e758ca8d
Jochen Topf <jochen@topf.org>
2018-07-20 11:37:29 +0200

    Use our internal functions for writing out locations in box output.
    This make box output consistent with the output of a single location
    and avoids problems with differen locales.

--- include/osmium/osm/box.hpp
+++ include/osmium/osm/box.hpp
@@ -223,15 +223,11 @@ namespace osmium {
     template <typename TChar, typename TTraits>
     inline std::basic_ostream<TChar, TTraits>& operator<<(std::basic_ostream<TChar, TTraits>& out, const osmium::Box& box) {
         if (box) {
-            out << '('
-                << box.bottom_left().lon()
-                << ','
-                << box.bottom_left().lat()
-                << ','
-                << box.top_right().lon()
-                << ','
-                << box.top_right().lat()
-                << ')';
+            out << '(';
+            box.bottom_left().as_string_without_check(std::ostream_iterator<char>(out));
+            out << ',';
+            box.top_right().as_string_without_check(std::ostream_iterator<char>(out));
+            out << ')';
         } else {
             out << "(undefined)";
         }
