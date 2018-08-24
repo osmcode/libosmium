02b2ebde1d4446051b1dbb817170abb0e96e31fc
Michael Reichert <michael.reichert@geofabrik.de>
2018-08-24 17:32:26 +0200

    Add DenseMemArray and run DenseMmapArray only on Linux
    mmap is only supported on Linux.

--- test/t/index/test_dump_and_load_index.cpp
+++ test/t/index/test_dump_and_load_index.cpp
@@ -3,17 +3,18 @@
 #include <osmium/index/detail/tmpfile.hpp>
 #include <osmium/index/map/dense_file_array.hpp>
 #include <osmium/index/map/dense_mmap_array.hpp>
+#include <osmium/index/map/dense_mem_array.hpp>
 #include <osmium/osm/location.hpp>
 #include <osmium/osm/types.hpp>
 
-using dense_mmap_array = osmium::index::map::DenseMmapArray<osmium::unsigned_object_id_type, osmium::Location>;
+using dense_mem_array = osmium::index::map::DenseMemArray<osmium::unsigned_object_id_type, osmium::Location>;
 using dense_file_array = osmium::index::map::DenseFileArray<osmium::unsigned_object_id_type, osmium::Location>;
 
-TEST_CASE("Dump DenseMmapArray, load as DenseFileArray") {
+template <class TMemoryIndex>
+void test_index() {
     const int fd = osmium::detail::create_tmp_file();
     REQUIRE(osmium::file_size(fd) == 0);
     constexpr const size_t MIN_INDEX_SIZE = sizeof(dense_file_array::element_type);
-
     const osmium::unsigned_object_id_type id1 = 12;
     const osmium::unsigned_object_id_type id2 = 3;
     const osmium::unsigned_object_id_type id3 = 7;
@@ -21,7 +22,7 @@ TEST_CASE("Dump DenseMmapArray, load as DenseFileArray") {
     const osmium::Location loc2{3.5, -7.2};
     const osmium::Location loc3{-12.7, 14.5};
 
-    dense_mmap_array index;
+    TMemoryIndex index;
     index.set(id1, loc1);
     index.set(id2, loc2);
     index.set(id3, loc3);
@@ -42,4 +43,18 @@ TEST_CASE("Dump DenseMmapArray, load as DenseFileArray") {
     REQUIRE_THROWS_AS(file_index.get(5), const osmium::not_found&);
     REQUIRE_THROWS_AS(file_index.get(6), const osmium::not_found&);
     REQUIRE_THROWS_AS(file_index.get(200), const osmium::not_found&);
+}
+
+#ifdef __linux__
+using dense_mmap_array = osmium::index::map::DenseMmapArray<osmium::unsigned_object_id_type, osmium::Location>;
+
+TEST_CASE("Dump DenseMmapArray, load as DenseFileArray") {
+    test_index<dense_mmap_array>();
+}
+#else
+# pragma message("not running 'DenseMmapArray' test case on this machine")
+#endif
+
+TEST_CASE("Dump DenseMemArray, load as DenseFileArray") {
+    test_index<dense_mem_array>();
 }
