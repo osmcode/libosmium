6fea0e82ac9f39584d8f6cbd4c1032af8c9ed4d9
Michael Reichert <michael.reichert@geofabrik.de>
2018-08-29 10:58:02 +0200

    tidy up unit test
    Use vector instead of a bunch of variables for the data.

--- test/t/index/test_dump_sparse_as_array.cpp
+++ test/t/index/test_dump_sparse_as_array.cpp
@@ -8,7 +8,7 @@
 #include <osmium/osm/types.hpp>
 #include <osmium/util/file.hpp>
 
-#include <iterator>
+#include <vector>
 
 using sparse_mmap_array = osmium::index::map::SparseMmapArray<osmium::unsigned_object_id_type, osmium::Location>;
 using dense_file_array = osmium::index::map::DenseFileArray<osmium::unsigned_object_id_type, osmium::Location>;
@@ -16,51 +16,61 @@ using dense_file_array = osmium::index::map::DenseFileArray<osmium::unsigned_obj
 TEST_CASE("Dump SparseMmapArray as array and load it as DenseFileArray") {
 
     const int fd = osmium::detail::create_tmp_file();
-    const constexpr size_t BUFFER_SIZE = 10L * 1024L * 1024L / sizeof(sparse_mmap_array::element_type::second_type);
+    constexpr const size_t VALUE_SIZE = sizeof(sparse_mmap_array::element_type::second_type);
+    constexpr const size_t BUFFER_SIZE = (10L * 1024L * 1024L) / VALUE_SIZE;
 
     REQUIRE(osmium::file_size(fd) == 0);
 
-    const osmium::unsigned_object_id_type id1 = 1;
-    const osmium::unsigned_object_id_type id2 = 6;
-    const osmium::unsigned_object_id_type id3 = 2 * BUFFER_SIZE;
-    const osmium::unsigned_object_id_type id4 = id3 + 1;
-    const osmium::unsigned_object_id_type id5 = 3 * BUFFER_SIZE - 1;
-    const osmium::unsigned_object_id_type id6 = 3 * BUFFER_SIZE;
-    const osmium::Location loc1{1.2, 4.5};
-    const osmium::Location loc2{3.5, -7.2};
-    const osmium::Location loc3{10.2, 64.5};
-    const osmium::Location loc4{39.5, -71.2};
-    const osmium::Location loc5{-1.2, 54.6};
-    const osmium::Location loc6{-9.3, 171.2};
+    std::vector<osmium::unsigned_object_id_type> ids = {
+            1,
+            6,
+            2 * BUFFER_SIZE,
+            2 * BUFFER_SIZE + 1,
+            3 * BUFFER_SIZE - 1,
+            3 * BUFFER_SIZE,
+            3 * BUFFER_SIZE + 1,
+            3 * BUFFER_SIZE + 2,
+            3 * BUFFER_SIZE + 3,
+            3 * BUFFER_SIZE + 4,
+            3 * BUFFER_SIZE + 5
+    };
+    std::vector<osmium::Location> locations = {
+            {1.2, 4.5},
+            {3.5, -7.2},
+            {10.2, 64.5},
+            {39.5, -71.2},
+            {-1.2, 54.6},
+            {-171.2, 9.3},
+            {-171.21, 9.26},
+            {-171.22, 9.25},
+            {-171.23, 9.24},
+            {-171.24, 9.23},
+            {-171.25, 9.22},
+            {-171.26, 9.21},
+    };
 
     constexpr const size_t S = sizeof(dense_file_array::element_type);
     sparse_mmap_array sparse_index;
-    sparse_index.set(id1, loc1);
-    sparse_index.set(id2, loc2);
-    sparse_index.set(id3, loc3);
-    sparse_index.set(id4, loc4);
-    sparse_index.set(id5, loc5);
-    sparse_index.set(id6, loc6);
+    for (size_t i = 0; i != ids.size() && i != locations.size(); ++i) {
+        sparse_index.set(ids[i], locations[i]);
+    }
     sparse_index.sort();
     sparse_index.dump_as_array(fd);
 
     dense_file_array dense_index{fd};
-    REQUIRE(osmium::file_size(fd) >= (id6 * S));
+    REQUIRE(osmium::file_size(fd) >= (*std::max_element(ids.begin(), ids.end()) * S));
     REQUIRE_THROWS_AS(dense_index.get(0), const osmium::not_found&);
-    REQUIRE_THROWS_AS(dense_index.get((BUFFER_SIZE - 1) / sizeof(osmium::Location)), const osmium::not_found&);
-    REQUIRE_THROWS_AS(dense_index.get(BUFFER_SIZE / sizeof(osmium::Location)), const osmium::not_found&);
-    REQUIRE_THROWS_AS(dense_index.get(id2 - 1), const osmium::not_found&);
-    REQUIRE_THROWS_AS(dense_index.get(id2 + 1), const osmium::not_found&);
+    REQUIRE_THROWS_AS(dense_index.get(BUFFER_SIZE - 1), const osmium::not_found&);
+    REQUIRE_THROWS_AS(dense_index.get(BUFFER_SIZE), const osmium::not_found&);
+    REQUIRE_THROWS_AS(dense_index.get(ids[1] - 1), const osmium::not_found&);
+    REQUIRE_THROWS_AS(dense_index.get(ids[1] + 1), const osmium::not_found&);
     REQUIRE_THROWS_AS(dense_index.get(100), const osmium::not_found&);
-    REQUIRE_THROWS_AS(dense_index.get(id3 - 1), const osmium::not_found&);
-    REQUIRE_THROWS_AS(dense_index.get(BUFFER_SIZE) / sizeof(osmium::Location), const osmium::not_found&);
+    REQUIRE_THROWS_AS(dense_index.get(ids[2] - 1), const osmium::not_found&);
+    REQUIRE_THROWS_AS(dense_index.get(BUFFER_SIZE), const osmium::not_found&);
     REQUIRE_THROWS_AS(dense_index.get(4 * BUFFER_SIZE), const osmium::not_found&);
     REQUIRE_THROWS_AS(dense_index.get(4 * BUFFER_SIZE - 1), const osmium::not_found&);
 
-    REQUIRE(dense_index.get(id1) == loc1);
-    REQUIRE(dense_index.get(id2) == loc2);
-    REQUIRE(dense_index.get(id3) == loc3);
-    REQUIRE(dense_index.get(id4) == loc4);
-    REQUIRE(dense_index.get(id5) == loc5);
-    REQUIRE(dense_index.get(id6) == loc6);
+    for (size_t i = 0; i != ids.size() && i != locations.size(); ++i) {
+        REQUIRE(dense_index.get(ids[i]) == locations[i]);
+    }
 }
