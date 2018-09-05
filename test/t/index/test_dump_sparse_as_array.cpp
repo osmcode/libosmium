a76385a2e462a09ae25a6aa8899c2d2a1bfad083
Michael Reichert <michael.reichert@geofabrik.de>
2018-09-05 13:39:39 +0200

    simplify test

--- test/t/index/test_dump_sparse_as_array.cpp
+++ test/t/index/test_dump_sparse_as_array.cpp
@@ -4,7 +4,7 @@
 #include <osmium/index/map/dense_file_array.hpp>
 #include <osmium/index/map/sparse_mmap_array.hpp>
 #include <osmium/index/node_locations_map.hpp>
-#include <osmium/osm/location.hpp>
+#include <osmium/osm/node_ref.hpp>
 #include <osmium/osm/types.hpp>
 #include <osmium/util/file.hpp>
 
@@ -21,56 +21,42 @@ TEST_CASE("Dump SparseMmapArray as array and load it as DenseFileArray") {
 
     REQUIRE(osmium::file_size(fd) == 0);
 
-    std::vector<osmium::unsigned_object_id_type> ids = {
-            1,
-            6,
-            2 * BUFFER_SIZE,
-            2 * BUFFER_SIZE + 1,
-            3 * BUFFER_SIZE - 1,
-            3 * BUFFER_SIZE,
-            3 * BUFFER_SIZE + 1,
-            3 * BUFFER_SIZE + 2,
-            3 * BUFFER_SIZE + 3,
-            3 * BUFFER_SIZE + 4,
-            3 * BUFFER_SIZE + 5
-    };
-    std::vector<osmium::Location> locations = {
-            {1.2, 4.5},
-            {3.5, -7.2},
-            {10.2, 64.5},
-            {39.5, -71.2},
-            {-1.2, 54.6},
-            {-171.2, 9.3},
-            {-171.21, 9.26},
-            {-171.22, 9.25},
-            {-171.23, 9.24},
-            {-171.24, 9.23},
-            {-171.25, 9.22},
-            {-171.26, 9.21},
+    std::vector<osmium::NodeRef> refs = {
+            osmium::NodeRef{1,                   osmium::Location{1.2, 4.5}},
+            osmium::NodeRef{6,                   osmium::Location{3.5, -7.2}},
+            osmium::NodeRef{2 * BUFFER_SIZE,     osmium::Location{10.2, 64.5}},
+            osmium::NodeRef{2 * BUFFER_SIZE + 1, osmium::Location{39.5, -71.2}},
+            osmium::NodeRef{3 * BUFFER_SIZE - 1, osmium::Location{-1.2, 54.6}},
+            osmium::NodeRef{3 * BUFFER_SIZE,     osmium::Location{-171.2, 9.3}},
+            osmium::NodeRef{3 * BUFFER_SIZE + 1, osmium::Location{-171.21, 9.26}},
+            osmium::NodeRef{3 * BUFFER_SIZE + 2, osmium::Location{-171.22, 9.25}},
+            osmium::NodeRef{3 * BUFFER_SIZE + 3, osmium::Location{-171.24, 9.23}},
+            osmium::NodeRef{3 * BUFFER_SIZE + 4, osmium::Location{-171.25, 9.22}},
+            osmium::NodeRef{3 * BUFFER_SIZE + 5, osmium::Location{-171.26, 9.21}}
     };
 
     constexpr const size_t S = sizeof(dense_file_array::element_type);
     sparse_mmap_array sparse_index;
-    for (size_t i = 0; i != ids.size() && i != locations.size(); ++i) {
-        sparse_index.set(ids[i], locations[i]);
+    for (auto r : refs) {
+        sparse_index.set(r.ref(), r.location());
     }
     sparse_index.sort();
     sparse_index.dump_as_array(fd);
 
     dense_file_array dense_index{fd};
-    REQUIRE(osmium::file_size(fd) >= (*std::max_element(ids.begin(), ids.end()) * S));
+    REQUIRE(osmium::file_size(fd) >= (std::max_element(refs.begin(), refs.end())->ref() * S));
     REQUIRE_THROWS_AS(dense_index.get(0), const osmium::not_found&);
     REQUIRE_THROWS_AS(dense_index.get(BUFFER_SIZE - 1), const osmium::not_found&);
     REQUIRE_THROWS_AS(dense_index.get(BUFFER_SIZE), const osmium::not_found&);
-    REQUIRE_THROWS_AS(dense_index.get(ids[1] - 1), const osmium::not_found&);
-    REQUIRE_THROWS_AS(dense_index.get(ids[1] + 1), const osmium::not_found&);
+    REQUIRE_THROWS_AS(dense_index.get(refs[1].ref() - 1), const osmium::not_found&);
+    REQUIRE_THROWS_AS(dense_index.get(refs[1].ref() + 1), const osmium::not_found&);
     REQUIRE_THROWS_AS(dense_index.get(100), const osmium::not_found&);
-    REQUIRE_THROWS_AS(dense_index.get(ids[2] - 1), const osmium::not_found&);
+    REQUIRE_THROWS_AS(dense_index.get(refs[2].ref() - 1), const osmium::not_found&);
     REQUIRE_THROWS_AS(dense_index.get(BUFFER_SIZE), const osmium::not_found&);
     REQUIRE_THROWS_AS(dense_index.get(4 * BUFFER_SIZE), const osmium::not_found&);
     REQUIRE_THROWS_AS(dense_index.get(4 * BUFFER_SIZE - 1), const osmium::not_found&);
 
-    for (size_t i = 0; i != ids.size() && i != locations.size(); ++i) {
-        REQUIRE(dense_index.get(ids[i]) == locations[i]);
+    for (auto r : refs) {
+        REQUIRE(dense_index.get(r.ref()) == r.location());
     }
 }
