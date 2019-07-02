aa91d6ee938bcfae874df366b51f390ed24acd45
Andreas Neumann <andr.neumann@googlemail.com>
2019-07-02 10:52:15 +0200

    Update projection.hpp

--- include/osmium/geom/projection.hpp
+++ include/osmium/geom/projection.hpp
@@ -68,7 +68,7 @@ namespace osmium {
             }; // struct ProjCRSDeleter
 
             std::unique_ptr<PJconsts, ProjCRSDeleter> m_crs;
-            const char* m_crs_char;
+            std::unique_ptr<const char*> m_crs_char;
 
         public:
 
@@ -77,7 +77,7 @@ namespace osmium {
                     if (!m_crs) {
                         throw osmium::projection_error{std::string{"creation of CRS failed: "} + proj_errno_string(proj_errno(proj_create(PJ_DEFAULT_CTX, crs)))};
                     }
-                    m_crs_char = crs;
+                    m_crs_char(crs);
             }
 
             explicit CRS(const std::string& crs) :
@@ -99,7 +99,7 @@ namespace osmium {
              * Get underlying projPJ handle from proj library.
              */
             const char* get_crs() const noexcept {
-                return m_crs_char;
+                return *m_crs_char.get();
             }
 
             bool is_latlong() const noexcept {
