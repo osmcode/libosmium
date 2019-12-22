7d85f5538cb02a4fd5ef37bdcb6fecf9172dc587
Jochen Topf <jochen@topf.org>
2019-12-22 11:14:46 +0100

    Make haversine function take a NodeRefList instead of WayNodeList
    WayNodeList derives from NodeRefList, so this is an easy change making
    the haversine function slightly more useful.

--- include/osmium/geom/haversine.hpp
+++ include/osmium/geom/haversine.hpp
@@ -35,8 +35,7 @@ DEALINGS IN THE SOFTWARE.
 
 #include <osmium/geom/coordinates.hpp>
 #include <osmium/geom/util.hpp>
-#include <osmium/osm/node_ref.hpp>
-#include <osmium/osm/way.hpp>
+#include <osmium/osm/node_ref_list.hpp>
 
 #include <cmath>
 #include <iterator>
@@ -73,13 +72,13 @@ namespace osmium {
             }
 
             /**
-             * Calculate length of way.
+             * Calculate length of node list (for instance from a way).
              */
-            inline double distance(const osmium::WayNodeList& wnl) {
+            inline double distance(const osmium::NodeRefList& nrl) {
                 double sum_length = 0;
 
-                for (auto it = wnl.begin(); it != wnl.end(); ++it) {
-                    if (std::next(it) != wnl.end()) {
+                for (auto it = nrl.begin(); it != nrl.end(); ++it) {
+                    if (std::next(it) != nrl.end()) {
                         sum_length += distance(it->location(), std::next(it)->location());
                     }
                 }
