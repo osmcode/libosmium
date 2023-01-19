a4ea3fee45c7c55d49fe3d94548b9248fd2ed083
Dmitry Marakasov <amdmi3@amdmi3.ru>
2023-01-19 19:41:33 +0300

    Enable posix_fadvise usage on FreeBSD

--- include/osmium/io/detail/read_write.hpp
+++ include/osmium/io/detail/read_write.hpp
@@ -253,7 +253,7 @@ namespace osmium {
              * needed again soon. Keeps the buffer cache clear for other
              * things.
              */
-#ifdef __linux__
+#if defined(__linux__) || defined(__FreeBSD__)
             inline void remove_buffered_pages(int fd) noexcept {
                 if (fd > 0) {
                     ::posix_fadvise(fd, 0, 0, POSIX_FADV_DONTNEED);
@@ -269,7 +269,7 @@ namespace osmium {
              * file that will not be needed again soon. Keeps the buffer cache
              * clear for other things.
              */
-#ifdef __linux__
+#if defined(__linux__) || defined(__FreeBSD__)
             inline void remove_buffered_pages(int fd, std::size_t size) noexcept {
                 constexpr const std::size_t block_size = 4096;
                 // Make sure to keep the last 10 blocks around, so were are
